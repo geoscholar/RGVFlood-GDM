64f07111a60d753b085acf0f997b99cff89ad10c
stacey.lyle@geoscholar <37156570+geoscholar@users.noreply.github.com>
2022-02-22 15:21:01 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Geospatial Data Model'
+copyright = '2022, RATES'
+author = 'Dr. Stacey D. Lyle'
 
 release = '0.1'
 version = '0.1.0'
